28a7ffc9153ca82540cd94fb64b201db19774465
galenye <galenye@tencent.com>
2019-03-23 20:55:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# tencent-cloud-cos-upload-image
+
+
+VSCode 插件，支持直接在 Markdown 文件中 **粘贴截图** 和 **选择图片** 上传到腾讯云COS，得到文件地址后引用显示。
+此插件拓展了 vscode-upload-tencentcos 的功能，它只支持了选择文件上传，但缺乏 Markdown 中常见的粘贴截图。
+
+---
+
+## 使用
+
+在 Markdown 文件中
+* 使用 shift + p, 粘贴板里面的图片会直接上传至cos
+* 使用 shift + o, 选择本地文件上传至cos
+
+## 参数配置
+
+```json
+{
+    // 地区，在COS对象存储中bucket对应的地域
+    "tencentCOSUpload.region": "",    
+    // secretId
+    "tencentCOSUpload.secretId": "",
+    // secretKey
+    "tencentCOSUpload.secretKey": "",
+    // 输入你的bucket名称，如 a-1250000000
+    "tencentCOSUpload.bucket": "",
+    // remotePath，您的存储目录，例如要把文件存在 http://${你的yuming}/images/png 这个目录下，则这里填写images/png）
+    "tencentCOSUpload.remotePath": "",
+    // 存储桶是否为公有可访问，如为私有，且希望上传后的url带签名，则设置为false，默认是 true
+    "tencentCOSUpload.isPublic": "true",
+    // 签名有效期，单位为妙，isPublic设置为 false 时有效
+    "tencentCOSUpload.duration": "31536000",
+    // 自定义域名，原来替换引用的默认COS域名
+    "tencentCOSUpload.domain": "",
+    // 临时目录，默认 /tmp/.tencentCOSUpload
+    "tencentCOSUpload.localPath": "/tmp/.tencentCOSUpload"
+}
+```
+
+-----------------------------------------------------------------------------------------------------------
+
+
+### Reference
+
+* [vscode-aliyun-upload-image](https://github.com/vvkee/vscode-aliyun-upload-image)
+* [vscode-upload-tencentCOS](https://github.com/Sean10/vscode-upload-tencentCOS)
+
+**Enjoy!**
---
+++ src/cos.ts
@@ -0,0 +1,95 @@
+import * as vscode from 'vscode'
+import * as path from 'path'
+import * as fs from 'fs'
+import * as url from 'url'
+import * as crypto from 'crypto'
+import * as moment from 'moment'
+const COS = require('cos-nodejs-sdk-v5')
+
+let cos: any = null
+
+const initInstance = (config: vscode.WorkspaceConfiguration) => {
+    cos = new COS({
+        SecretId: config.secretId,
+        SecretKey: config.secretKey,
+
+    })
+    return cos
+}
+
+const getMD5 = (imagePath: string) => {
+    return new Promise((resolve, reject) => {
+        var rs = fs.createReadStream(imagePath);
+        var hash = crypto.createHash('md5');
+        rs.on('data', hash.update.bind(hash));
+        rs.on('end', function () {
+          resolve(hash.digest('hex'));
+        });
+        rs.on('error', function(err) {
+            reject(err);
+        });
+    });
+}
+
+export default function (config: vscode.WorkspaceConfiguration, imagePath: string, selectFilePath?: string) {
+    let localFile = selectFilePath || imagePath,
+        bucket = config.bucket,
+        region = config.region,
+        domain = config.domain
+
+
+    if (domain && /^http(s)?\:\/\/(.+)/.test(domain)) {
+        domain = domain.replace(/^http(s)?\:\/\//, '')
+    }
+
+    if (/^".+"$/.test(localFile)) {
+        localFile = imagePath.substring(1, imagePath.length - 1)
+    }
+
+    return new Promise((resolve, reject) => {
+        getMD5(localFile)
+            .then((md5) => {
+                const remoteFile = path.join(config.remotePath, `${moment().format('YYYYMMDDHHmmss')}_${md5}.png`)
+                cos = cos || initInstance(config)
+
+                cos.putObject({
+                    Bucket: bucket,
+                    Region: region,
+                    Key: remoteFile,
+                    Body: fs.readFileSync(path.resolve(__dirname, localFile))
+                }, (err: any, data: any) => {
+
+                    // delete temp file
+                    if (!localFile) {
+                        fs.unlink(imagePath, err => {
+                            console.log("Failed to delete the temp image.")
+                        })
+                    }
+
+                    let url = cos.getObjectUrl({
+                        Bucket: bucket,
+                        Region: region,
+                        Key: remoteFile,
+                        Sign: !config.isPublic,
+                        Expires: config.duration
+                    })
+                    console.log(url)
+                    if (domain) {
+                        url = url.replace(/[^\/]+\.cos\..+\.myqcloud.com/, domain)
+                    }
+
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve({
+                            name: remoteFile,
+                            url: url
+                        })
+                    }
+                })
+            })
+            .catch((err) => {
+                reject(err)
+            })
+    })
+}
